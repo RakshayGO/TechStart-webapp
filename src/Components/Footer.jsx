c7a775d5bd2b1e17759611cfea3235e3d42621f7
RakshayGO <58980397+RakshayGO@users.noreply.github.com>
2023-01-09 05:38:19 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
         <div>
-            <p>Copyright &copy; TechStar 2023</p>
+            <p>Copyright &copy; TechStar 2023. Made by Rakshay</p>
         </div>
     </footer>
   )
